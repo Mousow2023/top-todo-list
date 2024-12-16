707741c2832b9fab2a4f2108af9fcad7cfdfe93c
Moustapha SOW <mousow123@gmail.com>
2024-12-16 14:01:57 +0000

    Basic overall design

--- src/index.js
+++ src/index.js
@@ -1,3 +1,84 @@
 import "./styles.css";
+import "./reset.css";
 
-console.log("It is working");
+class Project {
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+const listOfProject = [];
+
+// The Todo object
+class Todo {
+    static priorities = ["low", "medium", "high"];
+
+    constructor(title, description, dueDate, priority, project) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.project = project;
+    }
+}
+
+const defaultProject = new Project("Default");
+listOfProject.push(defaultProject);
+
+
+// Probably from the todo form
+let title = "title";
+let description = "";
+let dueDate = "20-12-2024";
+let priority = "High";
+let project = "";
+
+const testValues = [title, description, dueDate, priority, project];
+
+const todo = new Todo(...testValues);
+
+function valuesAreValid(todoList) {
+    // Check if the project is not empty and valid
+    if (todoList.project === "") {
+        todoList.project = defaultProject;
+    } else {
+        if (!listOfProject.includes(todoList.project)) {
+            alert("Invalid project");
+            return false;
+        }
+    }
+
+    // Check if the name is not empty
+    if (todoList.title === "") {
+        // alert("Invalid title");
+        return false;
+    }
+
+    // Check if the due date is a valid date and is not a past date
+    if (todoList.dueDate === "") {
+        // alert("Invalid dueDate");
+        return false;
+    }
+
+    // Check if the priority is in the list of prioties
+    if (!todoList.priorities.includes(todo.priority.toLowerCase())) {
+        // alert("Invalid priority");
+        return false;
+    }
+
+    return true;
+}
+
+console.log(valuesAreValid(todo))
+
+function createTodo() {
+    if (valuesAreValid(todo)) {
+        return todo;
+    } else {
+        return false;
+    }
+}
+
+console.log(createTodo());
+
+
