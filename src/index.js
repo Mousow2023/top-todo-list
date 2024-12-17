bf9c4e23c987adb62942962bc2af8497670a9dea
Moustapha SOW <mousow123@gmail.com>
2024-12-17 20:55:11 +0000

    Make the + button display the project dialog form

--- src/index.js
+++ src/index.js
@@ -37,48 +37,58 @@ const testValues = [title, description, dueDate, priority, project];
 
 const todo = new Todo(...testValues);
 
-function valuesAreValid(todoList) {
-    // Check if the project is not empty and valid
-    if (todoList.project === "") {
-        todoList.project = defaultProject;
-    } else {
-        if (!listOfProject.includes(todoList.project)) {
-            alert("Invalid project");
-            return false;
-        }
-    }
+// function valuesAreValid(todoList) {
+//     // Check if the project is not empty and valid
+//     if (todoList.project === "") {
+//         todoList.project = defaultProject;
+//     } else {
+//         if (!listOfProject.includes(todoList.project)) {
+//             alert("Invalid project");
+//             return false;
+//         }
+//     }
 
-    // Check if the name is not empty
-    if (todoList.title === "") {
-        // alert("Invalid title");
-        return false;
-    }
+//     // Check if the name is not empty
+//     if (todoList.title === "") {
+//         // alert("Invalid title");
+//         return false;
+//     }
 
-    // Check if the due date is a valid date and is not a past date
-    if (todoList.dueDate === "") {
-        // alert("Invalid dueDate");
-        return false;
-    }
+//     // Check if the due date is a valid date and is not a past date
+//     if (todoList.dueDate === "") {
+//         // alert("Invalid dueDate");
+//         return false;
+//     }
 
-    // Check if the priority is in the list of prioties
-    if (!todoList.priorities.includes(todo.priority.toLowerCase())) {
-        // alert("Invalid priority");
-        return false;
-    }
+//     // Check if the priority is in the list of prioties
+//     if (!todoList.priorities.includes(todo.priority.toLowerCase())) {
+//         // alert("Invalid priority");
+//         return false;
+//     }
 
-    return true;
-}
+//     return true;
+// }
 
-console.log(valuesAreValid(todo))
+// console.log(valuesAreValid(todo))
 
-function createTodo() {
-    if (valuesAreValid(todo)) {
-        return todo;
-    } else {
-        return false;
-    }
-}
+// function createTodo() {
+//     if (valuesAreValid(todo)) {
+//         return todo;
+//     } else {
+//         return false;
+//     }
+// }
 
-console.log(createTodo());
+// console.log(createTodo());
 
+const dialog = document.querySelector(".dialog");
+const showDialogButton = document.querySelector(".new-project");
+const closeDialogButton = document.querySelector(".cancel-button");
 
+showDialogButton.addEventListener("click", () => {
+    dialog.showModal();
+});
+
+closeDialogButton.addEventListener("click", () => {
+    dialog.close();
+})
